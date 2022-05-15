49bec179274a283b170417eb8bb7f994e98b18ea
fumixer <fumixer@gmail.com>
2022-05-15 17:34:04 -0400

    second commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+
+//function fight() {
+//   var playerName = window.prompt("What is your robot's name?");
+//  }
+
+  //fight();
+
+//  var playerName = window.prompt ("What is your robot's name?")
+//NOthe lack of uotation marks around playerName
+//window.alert(playerName);
+
+
+var playerName = window.prompt("What is your robot's name?");
+console.log("Your robot, " + playerName + ", has won!");
+var name = "your name";
+console.log(name);
